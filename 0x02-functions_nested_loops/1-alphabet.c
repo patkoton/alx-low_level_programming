463d5a07a4b720ba002b4a7f598ee5da42dd7998
Patience Orukotan <patkoton@yahoo.com>
2022-04-12 07:05:05 -0700

    A function that checks for lowercase character

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet, in lowercase, followed by a new line
- * Return: Always 0
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new l
  */
 void print_alphabet(void)
 {
